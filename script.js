3af855f7afbee514e20e772be8c4fc40e9d3f2f6
Federico Villagra <federico.villagra@mi.unc.edu.ar>
2024-04-12 06:31:32 -0300

    adding automatic languages

--- script.js
+++ script.js
@@ -1,77 +1,119 @@
-//Menu hamburguesa responsive
+// --- Menu hamburguesa responsive ---
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+// ---------------------------------------------------------------------------------------------
 
-//nombre de la persona del portfolio
+// --- Nombre de la persona del portfolio ----
 let name_profile = "Federico Villagra";
 let elementos = document.querySelectorAll('.name-profile');
 elementos.forEach(function(elemento) {
     elemento.textContent = name_profile;
 });
+// ---------------------------------------------------------------------------------------------
 
-// Año actual para derechos reservados
+// --- Año actual footer ---
 let fechaActual = new Date();
 let anioActual = fechaActual.getFullYear();
-document.getElementById('anio-actual').textContent = anioActual + " ";
+document.getElementById('anio-actual').textContent = anioActual;
+// ---------------------------------------------------------------------------------------------
 
-/*
-// copiar mail
-const mail = document.querySelector('.mail');
-const copiarMail = () => {
-  navigator.clipboard.writeText(mail.textContent);
-  mostrarMensajeCopiado();
+// --- Construir los cuadros de experiencia ---
+// Definir un objeto con la estructura de datos de las experiencias
+const experiences = {
+  "Frontend Development": [
+    { technology: "HTML", level: "Experienced" },
+    { technology: "CSS", level: "Experienced" },
+    { technology: "JavaScript", level: "Experienced" },
+    { technology: "Bootstrap", level: "Intermediate" },
+    { technology: "Tailwind", level: "Experienced" },
+    { technology: "React", level: "Experienced" }
+  ],
+  "Backend Development": [
+    { technology: "Express JS", level: "Experienced" },
+    { technology: "Node JS", level: "Experienced" },
+    { technology: "MongoDB", level: "Experienced" },
+    { technology: "Git", level: "Intermediate" },
+    { technology: "PHP", level: "Experienced" },
+    { technology: "MySQL", level: "Experienced" }
+  ]
 };
-mail.addEventListener("click", copiarMail);
-const mostrarMensajeCopiado = () => {
-    // Crea el mensaje "copiado"
-    var mensajeCopiado = document.createElement('div');
-    mensajeCopiado.textContent = 'Copied';
-    mensajeCopiado.id = 'mensaje-copiado';
-    // Agrega el mensaje al cuerpo del documento
-    document.body.appendChild(mensajeCopiado);
-    // Obtiene la posición del cursor
-    var posX = event.clientX;
-    var posY = event.clientY;
-    // Establece la posición del mensaje cerca del cursor
-    mensajeCopiado.style.left = (posX + 10) + 'px'; // Añade 10px para separarlo del cursor
-    mensajeCopiado.style.top = (posY + 10) + 'px'; // Añade 10px para separarlo del cursor
-    // Muestra el mensaje
-    mensajeCopiado.style.display = 'block';
-    // Oculta el mensaje después de un tiempo
-    setTimeout(function() {
-        mensajeCopiado.style.display = 'none';
-    }, 500); // Oculta el mensaje después de 0.5 segundos
+
+// Obtener el contenedor de experiencia
+const experienceContainer = document.getElementById("experiencee");
+
+// Iterar sobre el objeto de experiencias
+for (const category in experiences) {
+  // Crear elementos HTML para cada categoría
+  const categoryContainer = document.createElement("div");
+  categoryContainer.classList.add("details-container");
+
+  const categoryTitle = document.createElement("h2");
+  categoryTitle.classList.add("experience-sub-title");
+  categoryTitle.textContent = category;
+
+  const articleContainer = document.createElement("div");
+  articleContainer.classList.add("article-container");
+
+  // Iterar sobre las tecnologías de cada categoría
+  experiences[category].forEach(tech => {
+    const article = document.createElement("article");
+
+    const img = document.createElement("img");
+    img.src = "./assets/checkmark.png";
+    img.alt = "Experience icon";
+    img.classList.add("icon");
+
+    const div = document.createElement("div");
+
+    const h3 = document.createElement("h3");
+    h3.textContent = tech.technology;
+
+    const p = document.createElement("p");
+    p.textContent = tech.level;
+
+    div.appendChild(h3);
+    div.appendChild(p);
+
+    article.appendChild(img);
+    article.appendChild(div);
+
+    articleContainer.appendChild(article);
+  });
+
+  // Agregar los elementos creados al contenedor de experiencia
+  categoryContainer.appendChild(categoryTitle);
+  categoryContainer.appendChild(articleContainer);
+  experienceContainer.appendChild(categoryContainer);
 }
-*/
+// ---------------------------------------------------------------------------------------------
 
-
-// ---  Contruir las cards con js ----
+// --- Contruir las cards con js ---
 // Datos de los proyectos
 const proyectos = [
   {
-      nombre: "E-commerce app",
+      nombre: "E-commerce App",
       imagen: "./assets/project-1.png",
       github: "https://github.com/fedevillagra/React.js",
       open: "https://proyecto-final-federico-villagra.vercel.app/"
   },
   {
-      nombre: "Weather app",
+      nombre: "Weather App",
       imagen: "./assets/project-2.png",
       github: "https://github.com/fedevillagra/weather-app-next.js",
       open: "https://weather-app-next-js-fedevillagra.vercel.app/"
   },
   {
-      nombre: "Shopping app",
+      nombre: "Shopping App",
       imagen: "./assets/project-3.png",
       github: "https://github.com/fedevillagra/Proyecto_Final_Backend",
       open: "https://backend-proyect-federicovillagra.koyeb.app/"
   },
   {
-      nombre: "Tic-Tac-Toe app",
+      nombre: "Tic-Tac-Toe App",
       imagen: "./assets/project-4.png",
       github: "https://github.com/fedevillagra/tic-tac-toe",
       open: "https://fv-tic-tac-toe.vercel.app/TicTacToe"
@@ -98,12 +140,12 @@ function construirCards() {
       // Crear título del proyecto
       const titulo = document.createElement('h2');
       titulo.textContent = proyecto.nombre;
-      titulo.classList.add('experience-sub-title', 'project-title');
+      titulo.classList.add('experience-sub-title', 'project-title', 'translate');
 
       // Crear contenedor de botones
       const btnContainer = document.createElement('div');
       btnContainer.classList.add('btn-container');
-      const btnGithub = crearBoton("Github", proyecto.github);
+      const btnGithub = crearBoton("GitHub", proyecto.github);
       const btnOpen = crearBoton("Open", proyecto.open);
       btnContainer.appendChild(btnGithub);
       btnContainer.appendChild(btnOpen);
@@ -129,9 +171,9 @@ function crearBoton(texto, enlace) {
 }
 // Llamar a la función para construir las cards
 construirCards();
+// ---------------------------------------------------------------------------------------------
 
-
-// Agregar el boton de darkmode a las dos navbar
+// --- Agregar el boton de Darkmode a las dos Navbar ---
 async function moverDivANavbar() {
   // Función para crear una promesa que se resuelve cuando el elemento existe en el DOM
   function waitForElement(selector) {
@@ -181,3 +223,89 @@ moverElementoSegunAnchoDePantalla();
 window.addEventListener('resize', moverElementoSegunAnchoDePantalla);
 }
 moverDivANavbar();
+// ---------------------------------------------------------------------------------------------
+
+// ---- Traducir WEB -----
+function translatePage(targetLanguage) {
+  const apiKey = 'AIzaSyCWIh_sFCHC-G67NRLFnGRqio53kG0m_Eo';
+
+  // Seleccionar todos los elementos con la clase "translate"
+  const elementsToTranslate = document.querySelectorAll('.translate');
+
+  // Array para almacenar las promesas de traducción
+  const translationPromises = [];
+
+  // Iterar sobre los elementos y traducir su texto
+  elementsToTranslate.forEach(element => {
+      const textToTranslate = element.textContent.trim();
+      if (textToTranslate !== '') {
+          // URL de la API de Traducción de Google
+          const apiUrl = `https://translation.googleapis.com/language/translate/v2?key=${apiKey}`;
+
+          // Parámetros de la solicitud
+          const params = {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({
+                  q: textToTranslate,
+                  target: targetLanguage
+              })
+          };
+
+          // Realizar la solicitud a la API de Traducción de Google
+          const translationPromise = fetch(apiUrl, params)
+              .then(response => response.json())
+              .then(data => {
+                  const translatedText = data.data.translations[0].translatedText;
+                  element.textContent = translatedText; // Actualizar el texto del elemento con la traducción
+              })
+              .catch(error => console.error('Error al traducir el texto:', error));
+
+          translationPromises.push(translationPromise);
+      }
+  });
+
+  // Esperar a que todas las traducciones se completen antes de continuar
+  Promise.all(translationPromises)
+      .then(() => console.log('Traducción completada'))
+      .catch(error => console.error('Error al traducir la página:', error));
+}
+
+// Obtener el idioma del navegador del usuario
+const userLanguage = navigator.language || navigator.userLanguage;
+
+// Traducir la página al idioma del usuario automáticamente
+(userLanguage!='en-US') && translatePage(userLanguage);
+// ---------------------------------------------------------------------------------------------
+
+/*
+// --- Copiar mail ---
+const mail = document.querySelector('.mail');
+const copiarMail = () => {
+  navigator.clipboard.writeText(mail.textContent);
+  mostrarMensajeCopiado();
+};
+mail.addEventListener("click", copiarMail);
+const mostrarMensajeCopiado = () => {
+    // Crea el mensaje "copiado"
+    var mensajeCopiado = document.createElement('div');
+    mensajeCopiado.textContent = 'Copied';
+    mensajeCopiado.id = 'mensaje-copiado';
+    // Agrega el mensaje al cuerpo del documento
+    document.body.appendChild(mensajeCopiado);
+    // Obtiene la posición del cursor
+    var posX = event.clientX;
+    var posY = event.clientY;
+    // Establece la posición del mensaje cerca del cursor
+    mensajeCopiado.style.left = (posX + 10) + 'px'; // Añade 10px para separarlo del cursor
+    mensajeCopiado.style.top = (posY + 10) + 'px'; // Añade 10px para separarlo del cursor
+    // Muestra el mensaje
+    mensajeCopiado.style.display = 'block';
+    // Oculta el mensaje después de un tiempo
+    setTimeout(function() {
+        mensajeCopiado.style.display = 'none';
+    }, 500); // Oculta el mensaje después de 0.5 segundos
+}
+*/ // ---------------------------------------------------------------------------------------------
